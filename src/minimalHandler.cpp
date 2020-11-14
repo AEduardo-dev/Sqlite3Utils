24ebe74473a49e4e652edeb4fe64e0d050a9501e
Angel Eduardo Vega Conde <vegconed@villafafila.com>
2020-11-14 15:19:04 +0100

    Tests compilation and installation using google test framework on config time

--- src/minimalHandler.cpp
+++ src/minimalHandler.cpp
@@ -18,7 +18,15 @@ int main(int argc, char const *argv[]) {
 
 		std::cout << "Select the operation to apply on " << myHandler.getDbPath() << '\n';
 
+		/* Declaration of the table's fields */
+		std::vector<handler::FieldDescription> table_definition = \
+		{{"ID",query::data::int_+query::data::primary_key+query::data::not_null}, \
+				{"AGE", query::data::int_+query::data::not_null}, \
+				{"PHONE", query::data::int_+query::data::null}, \
+				{"NAME",query::data::char_+query::data::len(50)+query::data::not_null}};
 
+		if(myHandler.createTable("NEWTABLE", table_definition) == EXIT_SUCCESS)
+			std::cout << "Funciona" << '\n';
 
 		return 0;
 }
